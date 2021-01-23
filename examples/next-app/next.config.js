b05448a549d8057a0b087a1feca3b8459bc4f645
Flavio Ribeiro <flavioribeiro.tech@gmail.com>
2021-01-23 12:29:03 -0300

    chore(examples): use material ui

--- examples/next-app/next.config.js
+++ examples/next-app/next.config.js
@@ -1,4 +1,8 @@
-const withJSNX = require('../../packages/with-jsnx')();
+const withJSNX = require('../../packages/with-jsnx')({
+  options: {
+    componentsPath: '@material-ui/core',
+  },
+});
 
 module.exports = withJSNX({
   pageExtensions: ['js', 'jsx', 'jsnx'],
